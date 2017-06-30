1873c4cb7182b891cd935fb6d01fce5270637af0
David Colwell <david@pixability.com>
2017-06-30 14:30:15 -0400

    Squashed 'packages/app/' changes from b67c67c..44d8353
    44d8353 subtree:app Updated Readme
    f91310c Publish
    8c7ea6b subtree:app added module name to index.js
    cc9bcd0 Publish
    e0e2b7c Publish
    f80fda9 subtree:app retrying a minor change to test independent versioning.
    e329ead Publish
    91a0000 subtree:app minor change to test independent versioning.
    07d90d6 Publish
    f94526a Publish
    520a766 Publish
    559302d first cross project dependency.  App project requires common-components project.
    d54ec91 made some more changes to subtree package app index.js file so we can play with versioning in a bit.
    0c89034 subtree packages/app : added eslint, prettier, husky lint-staged
    1489f65 added npm packages eslint, prettier, husky, and lint-staged
    
    git-subtree-dir: packages/app
    git-subtree-split: 44d83536bc402a5998253b9885299b268ae2fd04

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Multi-Repo Multi-Project: v2 - The App
-This is the app project that will become part of a larger managed solution with mutliple repos, multiple projects, and shared and cross project dependencies.
+This is the app project that will become part of a larger managed solution with mutliple repos, multiple projects, and shared and cross project dependencies.   
+
+This project has a dependency on multi-repo-multi-project-v2-common-components.
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("this is the app").
+const moduleName = "App";
+
+const str1 =
+  'this is the app project from repo: https://github.com/photusenigma/multi-repo-multi-project-v2-app';
+const srt2 = () => {
+  return 'it will should be worked on and managed as a subtree of https://github.com/photusenigma/multi-repo-multi-project-v2';
+};
+
+console.log(str1);
+console.log(srt2);
+
+
+const names = ['React', 'Angular', 'GraphQL', 'Redux'];
+const count = 100;
+const myFun = (name1, name2, count) => `Wrote ${count} lines with ${name1}, and ${Math.floor(count/2)} with ${name2}`
+const getRandomNumFromZeroTo = (num) => Math.floor(Math.random() * num)
+
+let myCount = count;
+let namesLen = names.length;
+while(myCount){
+  const name1 = names[getRandomNumFromZeroTo(namesLen)]
+  const name2 = names[getRandomNumFromZeroTo(namesLen)]
+  const myNames = [name1, name2];
+  console.log(myFun(...myNames, myCount));
+  myCount--;
+}
